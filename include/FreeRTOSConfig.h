502ab5c13a19ad5ecb53594567a084320829a24e
udayapeddirajub <udaysrkrec@gmail.com>
2025-12-31 18:53:01 +0530

    fix: add FreeRTOSConfig and configure freertos_config target

---
+++ include/FreeRTOSConfig.h
@@ -0,0 +1,22 @@
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+#define configUSE_PREEMPTION                    1
+#define configUSE_IDLE_HOOK                     0
+#define configUSE_TICK_HOOK                     0
+#define configCPU_CLOCK_HZ                      ( ( unsigned long ) 1000000 )
+#define configTICK_RATE_HZ                      ( ( TickType_t ) 1000 )
+#define configMAX_PRIORITIES                    ( 5 )
+#define configMINIMAL_STACK_SIZE                ( ( unsigned short ) 128 )
+#define configTOTAL_HEAP_SIZE                   ( ( size_t ) ( 75 * 1024 ) )
+#define configMAX_TASK_NAME_LEN                 ( 16 )
+#define configUSE_TRACE_FACILITY                0
+#define configUSE_16_BIT_TICKS                  0
+#define configIDLE_SHOULD_YIELD                 1
+
+/* Set the following definitions to 1 to include the API function, or zero
+to exclude the API function. */
+#define vTaskDelay                              1
+#define xTaskCreate                             1
+
+#endif /* FREERTOS_CONFIG_H */
